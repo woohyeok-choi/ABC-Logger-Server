dff2f6ddee8aaf79d7f9feeffdcb7e19b17cfcf0
Woohyeok Choi <woohyeok.choi@kaist.ac.kr>
2020-02-05 18:29:39 +0900

    Release 0.9.3
    Change POSTGRE_XXX to POSTGRES_XXX

--- src/main/kotlin/kaist/iclab/abclogger/main.kt
+++ src/main/kotlin/kaist/iclab/abclogger/main.kt
@@ -2,20 +2,20 @@ package kaist.iclab.abclogger
 
 const val KEY_PORT_NUMBER = "PORT_NUMBER"
 const val KEY_LOG_PATH = "LOG_PATH"
-const val KEY_POSTGRE_USER = "POSTGRE_USER"
-const val KEY_POSTGRE_PASSWORD = "POSTGRE_PASSWORD"
-const val KEY_POSTGRE_SERVER_NAME = "POSTGRE_SERVER_NAME"
-const val KEY_POSTGRE_PORT_NUMBER = "POSTGRE_PORT_NUMBER"
-const val KEY_POSTGRE_DB_NAME = "POSTGRE_DB_NAME"
+const val KEY_POSTGRES_USER = "POSTGRES_USER"
+const val KEY_POSTGRES_PASSWORD = "POSTGRES_PASSWORD"
+const val KEY_POSTGRES_SERVER_NAME = "POSTGRES_SERVER_NAME"
+const val KEY_POSTGRES_PORT_NUMBER = "POSTGRES_PORT_NUMBER"
+const val KEY_POSTGRES_DB_NAME = "POSTGRES_DB_NAME"
 
 fun main() {
     val portNumber = System.getenv(KEY_PORT_NUMBER)?.toIntOrNull() ?: 50051
     val logPath = System.getenv(KEY_LOG_PATH) ?: "./log/abc.log"
-    val dbServerName = System.getenv(KEY_POSTGRE_SERVER_NAME) ?: "localhost"
-    val dbPortNumber = System.getenv(KEY_POSTGRE_PORT_NUMBER)?.toIntOrNull() ?: 5432
-    val dbName = System.getenv(KEY_POSTGRE_DB_NAME) ?: "postgres"
-    val dbUserName = System.getenv(KEY_POSTGRE_USER) ?: "postgre"
-    val dbPassword = System.getenv(KEY_POSTGRE_PASSWORD) ?: "postgre"
+    val dbServerName = System.getenv(KEY_POSTGRES_SERVER_NAME) ?: "localhost"
+    val dbPortNumber = System.getenv(KEY_POSTGRES_PORT_NUMBER)?.toIntOrNull() ?: 5432
+    val dbName = System.getenv(KEY_POSTGRES_DB_NAME) ?: "postgres"
+    val dbUserName = System.getenv(KEY_POSTGRES_USER) ?: "postgre"
+    val dbPassword = System.getenv(KEY_POSTGRES_PASSWORD) ?: "postgre"
 
     val app = App(
             portNumber = portNumber,
